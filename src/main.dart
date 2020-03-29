6e23c7842a5d8051958aae22ad209c84cb209d36
Brandon Burrus <brandon@burrus.io>
2020-03-29 12:46:10 -0700

    style: fixed formatting

--- src/main.dart
+++ src/main.dart
@@ -7,11 +7,15 @@ class TodosApp extends StatelessWidget {
   @override
   build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(title: Text('To-dos')),
-            body: SafeArea(
-                child: Container(
-                    padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-                    child: Todos()))));
+      home: Scaffold(
+        appBar: AppBar(title: Text('To-dos')),
+        body: SafeArea(
+          child: Container(
+            padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+            child: Todos(),
+          ),
+        ),
+      ),
+    );
   }
 }
